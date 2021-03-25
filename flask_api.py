35301072ad9b30fcab3b78cfa4aa85c471670ca3
tapann285 <tapann285@users.noreply.github.com>
2021-03-25 23:50:27 +0530

    Update flask_api.py

--- flask_api.py
+++ flask_api.py
@@ -362,9 +362,8 @@ def home():
 
    
     
-app.listen(process.env.PORT || 3000, function(){
-  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
-});
+var port_number = server.listen(process.env.PORT || 3000);
+app.listen(port_number);
 
 if __name__ == '__main__':
     app.run()
